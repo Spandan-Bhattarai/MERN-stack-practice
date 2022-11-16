75052161d2b0f585255fcbe687684cf8ea4274e5
Spandan Bhattarai <sbhattarai50@gmail.com>
2022-11-16 09:04:15 +0545

    third commit

--- Node/server.js
+++ Node/server.js
@@ -1,3 +1,9 @@
 // console.log(process.argv); showsa the path of files being executed
 
-ProcessingInstruction.exit(); //ends the program
+
+console.log(_filename);
+console.log(_dirname);
+
+
+
+process.exit(); //ends the program
